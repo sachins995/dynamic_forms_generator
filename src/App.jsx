19d24818b977badd69bf839de0592bc08d246b40
sachins995 <sachin.psl99@gmail.com>
2024-02-17 18:51:48 +0530

    updates

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import Demo from './demo'
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  
+  return (
+    <>
+      <Demo/>
+    </>
+  )
+}
+
+export default App
